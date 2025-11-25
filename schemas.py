3e2d2a31544cf0e986b82ffd2e62d525e8bc8145
Oriol Orbea <oorbea@pasiona.com>
2025-11-25 14:13:22 +0100

    Add ailments field to UserRegisterSchema for patient data

--- schemas.py
+++ schemas.py
@@ -9,3 +9,4 @@ class UserRegisterSchema(Schema):
     email = fields.Email(required=True, metadata={"description": "The email address of the user."})
     password = fields.String(required=True, load_only=True, metadata={"description": "The password for the user."})
     role = fields.Enum(UserType, required=False, load_default=UserType.PATIENT, dump_default=UserType.PATIENT, metadata={"description": "The role of the user."})
+    ailments = fields.String(required=False, allow_none=True, validate=lambda s: len(s) <= 1024, metadata={"description": "The ailments of the patient, if applicable."})
