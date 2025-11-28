18481e2107d59186a710aad385aacba75efec816
oorbea <oriol.orbea@gmail.com>
2025-11-28 18:18:36 +0100

    Add Question model with CRUD methods and update __init__.py exports

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,15 @@ from models.user import User
 from models.patient import Patient
 from models.doctor import Doctor
 from models.admin import Admin
-from models.associations import DoctorPatientAssociation
+from models.associations import DoctorPatientAssociation, UserCodeAssociation
+from models.questions import Question
 
 __all__ = [
     'User',
     'Patient',
     'Doctor',
     'Admin',
+    'Question',
     'DoctorPatientAssociation',
+    'UserCodeAssociation'
 ]
