c9f2a5dcbe33455347d6ef3f73e20c856c63204d
oorbea <oriol.orbea@gmail.com>
2025-11-30 12:37:25 +0100

    Add new exception class for SMTP credential errors

--- helpers/exceptions/mail_exceptions.py
+++ helpers/exceptions/mail_exceptions.py
@@ -1,3 +1,7 @@
 class SendEmailException(Exception):
     """Exception raised for errors in the email sending process."""
     pass
+
+class SMTPCredentialsException(Exception):
+    """Exception raised for SMTP credential related errors."""
+    pass
