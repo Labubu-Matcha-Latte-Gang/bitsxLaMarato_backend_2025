1ccab28a589fbfb6e7b680eb58a6abfb8c727a47
oorbea <oriol.orbea@gmail.com>
2025-11-30 14:34:43 +0100

    Refactor IUserRole class to inherit from ABC and ensure all methods are abstract

--- models/interfaces.py
+++ models/interfaces.py
@@ -1,10 +1,12 @@
+from abc import ABC, abstractmethod
 from helpers.enums.user_role import UserRole
 
 
-class IUserRole:
+class IUserRole(ABC):
     """
     Interface for user roles
     """
+    @abstractmethod
     def get_user(self):
         """
         Get the associated User object
@@ -13,6 +15,7 @@ class IUserRole:
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def get_email(self) -> str:
         """
         Get the email for this role
@@ -21,6 +24,7 @@ class IUserRole:
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def set_email(self, new_email: str) -> None:
         """
         Set a new email for this role
@@ -29,6 +33,7 @@ class IUserRole:
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def to_dict(self) -> dict:
         """
         Convert the role to a dictionary representation
@@ -37,6 +42,7 @@ class IUserRole:
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def get_role(self) -> UserRole:
         """
         Get the role of this user
@@ -45,12 +51,14 @@ class IUserRole:
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def remove_all_associations_between_user_roles(self) -> None:
         """
         Remove all associations between user roles for this role
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def set_properties(self, data: dict) -> None:
         """
         Set multiple properties for the role from a dictionary
@@ -59,6 +67,7 @@ class IUserRole:
         """
         raise NotImplementedError()
 
+    @abstractmethod
     def doctor_of_this_patient(self, patient) -> bool:
         """
         Checks if the role grants access to the provided patient
