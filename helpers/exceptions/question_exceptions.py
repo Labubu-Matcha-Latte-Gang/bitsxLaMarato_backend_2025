cc9828db0e95e3b85895b4f246943ad76994e22c
oorbea <oriol.orbea@gmail.com>
2025-11-30 17:18:51 +0100

    Refactor question exception classes for clarity and consistency

--- helpers/exceptions/question_exceptions.py
+++ helpers/exceptions/question_exceptions.py
@@ -5,3 +5,15 @@ class BaseQuestionException(Exception):
 class QuestionNotFoundException(BaseQuestionException):
     """Exception raised when a question is not found."""
     pass
+
+class QuestionCreationException(BaseQuestionException):
+    """Exception raised when question creation fails."""
+    pass
+
+class QuestionUpdateException(BaseQuestionException):
+    """Exception raised when updating a question fails."""
+    pass
+
+class QuestionDeletionException(BaseQuestionException):
+    """Exception raised when deleting a question fails."""
+    pass
