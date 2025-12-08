0f7ea6a3cda388a1f09fdf25c864e41aa4c8b51e
Oriol Orbea <oriol.orbea@gmail.com>
2025-12-08 14:15:38 +0100

    feat: add PDF generator adapter method to the adapter factory

--- helpers/factories/adapter_factories.py
+++ helpers/factories/adapter_factories.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from helpers.graphic_adapter import AbstractGraphicAdapter, SimplePlotlyAdapter
+from helpers.pdf_generator_adapter import AbstractPDFGeneratorAdapter, PDFGeneratorAdapter
 from helpers.qr_adapter import AbstractQRAdapter, QRAdapter
 
 
@@ -25,6 +26,11 @@ class AbstractAdapterFactory(ABC):
         """Return an instance of a QR code adapter."""
         raise NotImplementedError
     
+    @abstractmethod
+    def get_pdf_generator_adapter(self) -> AbstractPDFGeneratorAdapter:
+        """Return an instance of a PDF generator adapter."""
+        raise NotImplementedError
+    
 class AdapterFactory(AbstractAdapterFactory):
     """Concrete factory for creating adapter instances."""
 
@@ -33,3 +39,6 @@ class AdapterFactory(AbstractAdapterFactory):
     
     def get_qr_adapter(self) -> QRAdapter:
         return QRAdapter()
+    
+    def get_pdf_generator_adapter(self) -> PDFGeneratorAdapter:
+        return PDFGeneratorAdapter()
