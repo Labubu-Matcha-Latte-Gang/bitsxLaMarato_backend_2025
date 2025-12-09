69e3bc2cf1a56b026c7f49450704e45c925d99dd
Oriol Orbea <oorbea@pasiona.com>
2025-12-09 10:29:49 +0100

    feat: add custom exceptions for PDF generation errors

--- helpers/exceptions/pdf_exceptions.py
+++ helpers/exceptions/pdf_exceptions.py
@@ -1,3 +1,7 @@
 class PDFGenerationException(Exception):
     """Excepció personalitzada per errors en la generació de PDFs."""
     pass
+
+class InvalidZoneInfoException(PDFGenerationException):
+    """Excepció personalitzada per errors en la zona horària."""
+    pass
