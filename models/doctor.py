3e17282fb4367fad34afd515ed5f43c59eb2f834
oorbea <oriol.orbea@gmail.com>
2025-12-11 18:44:18 +0100

    Add gender field to Doctor model for enhanced data representation

--- models/doctor.py
+++ models/doctor.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from sqlalchemy.orm import Mapped
 
 from db import db
+from helpers.enums.gender import Gender
 from helpers.enums.user_role import UserRole
 from models.associations import DoctorPatientAssociation
 from models.user import User
@@ -22,6 +23,7 @@ class Doctor(User):
         db.ForeignKey("users.email", onupdate="CASCADE", ondelete="CASCADE"),
         primary_key=True,
     )
+    gender = db.Column(db.Enum(Gender), nullable=False)
     patients: Mapped[list["Patient"]] = db.relationship(
         "Patient",
         secondary=DoctorPatientAssociation.__table__,
